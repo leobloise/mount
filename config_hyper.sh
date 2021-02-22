56c6a55f5a82978e2836edcd755fa3eb5f55ae72
Leonardo Cardoso da Silva Bloise <leonardo.bloise@outlook.com>
2021-02-22 03:07:57 -0300

    Adicionando Hyper

---
+++ config_hyper.sh
@@ -0,0 +1,1 @@
+hyper install hyper-omni-theme
--- prepare.sh
+++ prepare.sh
@@ -60,6 +60,8 @@ if [ $? -eq 0 ]; then
             wget -O hyper_3.0.2.deb  https://releases.hyper.is/download/deb
             sudo gdebi ./hyper_3.0.2.deb 
 
+            bash ./config_hyper.sh
+
             wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
             echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
             sudo apt-get update
